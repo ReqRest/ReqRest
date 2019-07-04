fda6a69343979f3e192aa30bf0ee2442176a213d
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2019-07-04 23:13:34 +0200

    Finished StatusCodeRange tests.

---
+++ src/ReqRest.Http.Tests/StatusCodeRange/ToStringTests.cs
@@ -0,0 +1,22 @@
+﻿namespace ReqRest.Http.Tests.StatusCodeRange
+{
+    using FluentAssertions;
+    using ReqRest.Http;
+    using Xunit;
+
+    public class ToStringTests
+    {
+
+        [Theory]
+        [InlineData(null, null, "*")]
+        [InlineData(null, 200, "*-200")]
+        [InlineData(200, null, "200-*")]
+        [InlineData(200, 300, "200-300")]
+        public void Returns_Expected_String(int? from, int? to, string expected)
+        {
+            new StatusCodeRange(from, to).ToString().Should().Be(expected);
+        }
+
+    }
+
+}
