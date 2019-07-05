07ee77258f7d75f2fa593e8beff9ef65e9dc9d57
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2019-07-05 15:57:12 +0200

    Fixed root namespace and move extensions into own file (serializer)

--- src/ReqRest.Serializers/IHttpContentDeserializer.cs
+++ src/ReqRest.Serializers/IHttpContentDeserializer.cs
@@ -38,44 +38,4 @@
 
     }
 
-    /// <summary>
-    ///     Extends the <see cref="IHttpContentDeserializer"/> with default extension methods.
-    /// </summary>
-    public static class HttpContentDeserializerExtensions
-    {
-
-        /// <summary>
-        ///     Deserializes an object of the specified type <typeparamref name="T"/> from
-        ///     the <paramref name="httpContent"/>.
-        /// </summary>
-        /// <typeparam name="T">
-        ///     The target type of the object which is supposed to be deserialized.
-        /// </typeparam>
-        /// <param name="serializer">The serializer.</param>
-        /// <param name="httpContent">
-        ///     An <see cref="HttpContent"/> instance from which the content should be serialized.
-        /// </param>
-        /// <returns>
-        ///     An object of type <typeparamref name="T"/>.
-        /// </returns>
-        /// <exception cref="ArgumentNullException">
-        ///     * <paramref name="serializer"/>
-        ///     * <paramref name="httpContent"/>
-        /// </exception>
-        /// <exception cref="InvalidCastException">
-        ///     The serializer returned an object which is not of type <typeparamref name="T"/>.
-        /// </exception>
-        /// <exception cref="HttpContentSerializationException">
-        ///     Deserializing the content failed.
-        /// </exception>
-        public static async Task<T> DeserializeAsync<T>(this IHttpContentDeserializer serializer, HttpContent httpContent)
-        {
-#nullable disable
-            _ = serializer ?? throw new ArgumentNullException(nameof(serializer));
-            return (T)await serializer.DeserializeAsync(httpContent, typeof(T)).ConfigureAwait(false);
-#nullable restore
-        }
-
-    }
-
 }
