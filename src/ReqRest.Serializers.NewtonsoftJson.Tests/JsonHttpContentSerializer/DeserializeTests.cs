8e33d790ec688bffbacdb42bef75ff5036f1f6b0
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2019-07-14 21:30:44 +0200

    Added various Json tests.

---
+++ src/ReqRest.Serializers.NewtonsoftJson.Tests/JsonHttpContentSerializer/DeserializeTests.cs
@@ -0,0 +1,38 @@
+﻿namespace ReqRest.Serializers.NewtonsoftJson.Tests.JsonHttpContentSerializer
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using FluentAssertions;
+    using Newtonsoft.Json;
+    using ReqRest.Serializers.NewtonsoftJson;
+    using Xunit;
+
+    public class DeserializeTests
+    {
+
+        [Theory]
+        [MemberData(nameof(JsonTestData.ValidJson), MemberType = typeof(JsonTestData))]
+        public async Task Deserializes_Object(string json)
+        {
+            var content = new StringContent(json);
+            var serializer = new JsonHttpContentSerializer();
+            var deserialized = await serializer.DeserializeAsync(content, typeof(MockDto));
+            var expected = JsonConvert.DeserializeObject<MockDto>(json);
+            deserialized.Should().BeEquivalentTo(expected);
+        }
+        
+        [Theory]
+        [MemberData(nameof(JsonTestData.InvalidJson), MemberType = typeof(JsonTestData))]
+        public async Task Throws_HttpContentSerializationException_For_Invalid_Json(string json)
+        {
+            var content = new StringContent(json);
+            var serializer = new JsonHttpContentSerializer();
+
+            Func<Task> testCode = async () => await serializer.DeserializeAsync(content, typeof(MockDto));
+            await testCode.Should().ThrowAsync<HttpContentSerializationException>();
+        }
+
+    }
+
+}
