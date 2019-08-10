cfaf13955db8e10480436b7c8fdef580c131f29e
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2019-08-10 19:03:32 +0200

    Refactored the ApiRequest tests so that there are less classes.

--- src/ReqRest.Client.Tests/ApiRequest/ApiRequestTestBase.cs
+++ src/ReqRest.Client.Tests/ApiRequest/ApiRequestTestBase.cs
@@ -4,7 +4,7 @@ namespace ReqRest.Client.Tests.ApiRequest
     using System.Net.Http;
     using ReqRest.Client;
 
-    public abstract class ApiRequestTestBase<TRequest> where TRequest : ApiRequestBase
+    public abstract partial class ApiRequestTestBase<TRequest> where TRequest : ApiRequestBase
     {
 
         protected dynamic CreateDynamicRequest() =>
@@ -18,6 +18,54 @@ namespace ReqRest.Client.Tests.ApiRequest
         
         protected abstract TRequest CreateRequest(Func<HttpClient> httpClientProvider);
 
+    }
+
+    public class ApiRequestTests : ApiRequestTestBase<ApiRequest>
+    {
+        protected override ApiRequest CreateRequest(Func<HttpClient> httpClientProvider) =>
+            ApiRequestHelper.Create(httpClientProvider);
+    }
+
+    public class ApiRequestT1Tests : ApiRequestTestBase<ApiRequest<Dto1>>
+    {
+        protected override ApiRequest<Dto1> CreateRequest(Func<HttpClient> httpClientProvider) =>
+            ApiRequestHelper.CreateT1(httpClientProvider);
+    }
+
+    public class ApiRequestT2Tests : ApiRequestTestBase<ApiRequest<Dto1, Dto2>>
+    {
+        protected override ApiRequest<Dto1, Dto2> CreateRequest(Func<HttpClient> httpClientProvider) =>
+            ApiRequestHelper.CreateT2(httpClientProvider);
+    }
+
+    public class ApiRequestT3Tests : ApiRequestTestBase<ApiRequest<Dto1, Dto2, Dto3>>
+    {
+        protected override ApiRequest<Dto1, Dto2, Dto3> CreateRequest(Func<HttpClient> httpClientProvider) =>
+            ApiRequestHelper.CreateT3(httpClientProvider);
+    }
+
+    public class ApiRequestT4Tests : ApiRequestTestBase<ApiRequest<Dto1, Dto2, Dto3, Dto4>>
+    {
+        protected override ApiRequest<Dto1, Dto2, Dto3, Dto4> CreateRequest(Func<HttpClient> httpClientProvider) =>
+            ApiRequestHelper.CreateT4(httpClientProvider);
+    }
+
+    public class ApiRequestT5Tests : ApiRequestTestBase<ApiRequest<Dto1, Dto2, Dto3, Dto4, Dto5>>
+    {
+        protected override ApiRequest<Dto1, Dto2, Dto3, Dto4, Dto5> CreateRequest(Func<HttpClient> httpClientProvider) =>
+            ApiRequestHelper.CreateT5(httpClientProvider);
+    }
+
+    public class ApiRequestT6Tests : ApiRequestTestBase<ApiRequest<Dto1, Dto2, Dto3, Dto4, Dto5, Dto6>>
+    {
+        protected override ApiRequest<Dto1, Dto2, Dto3, Dto4, Dto5, Dto6> CreateRequest(Func<HttpClient> httpClientProvider) =>
+            ApiRequestHelper.CreateT6(httpClientProvider);
+    }
+
+    public class ApiRequestT7Tests : ApiRequestTestBase<ApiRequest<Dto1, Dto2, Dto3, Dto4, Dto5, Dto6, Dto7>>
+    {
+        protected override ApiRequest<Dto1, Dto2, Dto3, Dto4, Dto5, Dto6, Dto7> CreateRequest(Func<HttpClient> httpClientProvider) =>
+            ApiRequestHelper.CreateT7(httpClientProvider);
     }
 
 }
