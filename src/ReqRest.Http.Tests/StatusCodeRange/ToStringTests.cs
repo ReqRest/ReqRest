0ba518858a1b6e7ff3e7340f82d9a9a40785e510
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2019-08-31 00:36:04 +0200

    Updated the StatusCodeRange.ToString() format to [200, 300]. Updated the ResponseTypeInfo.ToString() method accordingly.

--- src/ReqRest.Http.Tests/StatusCodeRange/ToStringTests.cs
+++ src/ReqRest.Http.Tests/StatusCodeRange/ToStringTests.cs
@@ -10,9 +10,11 @@
         [Theory]
         [InlineData(null, null, "*")]
         [InlineData(200, 200, "200")]
-        [InlineData(null, 200, "*-200")]
-        [InlineData(200, null, "200-*")]
-        [InlineData(200, 300, "200-300")]
+        [InlineData(null, 200, "[*, 200]")]
+        [InlineData(200, null, "[200, *]")]
+        [InlineData(200, 300, "[200, 300]")]
+        [InlineData(-300, 200, "[-300, 200]")]
+        [InlineData(-300, -200, "[-300, -200]")]
         public void Returns_Expected_String(int? from, int? to, string expected)
         {
             new StatusCodeRange(from, to).ToString().Should().Be(expected);
