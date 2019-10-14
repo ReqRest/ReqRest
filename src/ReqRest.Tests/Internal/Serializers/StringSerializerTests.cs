8688ceee37109b3e73a5a29c2b0edcdb862f8333
Manuel Römer <30902964+manuelroemer@users.noreply.github.com>
2019-10-14 17:33:54 +0200

    Migrated StringSerializer tests.

---
+++ src/ReqRest.Tests/Internal/Serializers/StringSerializerTests.cs
@@ -0,0 +1,40 @@
+﻿namespace ReqRest.Tests.Internal.Serializers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using System.Linq;
+    using ReqRest.Internal.Serializers;
+    using Xunit;
+    using System.Net.Http;
+
+    public class StringSerializerTests
+    {
+
+        public class DeserializeTests
+        {
+
+            [Fact]
+            public async Task Throws_NotSupportedException_When_Deserializing_Other_Type()
+            {
+                var serializer = new StringSerializer();
+                await Assert.ThrowsAsync<NotSupportedException>(
+                    async () => await serializer.DeserializeAsync(new ByteArrayContent(Array.Empty<byte>()), typeof(int))
+                );
+            }
+
+            [Fact]
+            public async Task Deserializes_String()
+            {
+                var expected = "Hello World";
+                var serializer = new StringSerializer();
+                var deserialized = await serializer.DeserializeAsync(new StringContent(expected), typeof(string));
+                Assert.IsType<string>(deserialized);
+                Assert.Equal(expected, deserialized);
+            }
+
+        }
+
+    }
+
+}
